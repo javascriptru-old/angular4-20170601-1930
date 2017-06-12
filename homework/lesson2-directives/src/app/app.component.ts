c80c4a5041ac0cd3039a02b9f41a6428a486f594
e-watson <eduard.skripchenko@icloud.com>
2017-06-12 06:49:02 +0300

    Email date pipe implementation

--- homework/lesson2-directives/src/app/app.component.ts
+++ homework/lesson2-directives/src/app/app.component.ts
@@ -9,9 +9,9 @@ import { MailData } from 'app/models/mail-data';
 export class AppComponent {
     mailDataItems: Array<MailData> = [
         new MailData(1, 'Sherlock Holmes', 'The Hound of the Baskervilles', new Date()),
-        new MailData(2, 'John Watson', 'The Sign of the Four', this.getDateMinusMinutes(3)),
-        new MailData(3, 'James Moriarty', 'The Valley of Fear', this.getDateMinusMinutes(180)),
-        new MailData(4, 'Mrs. Hudson', 'A Scandal in Bohemia', this.getDateMinusMinutes(2880))
+        new MailData(2, 'John Watson', 'The Sign of the Four', this.getDateMinusMinutes(5)),
+        new MailData(3, 'James Moriarty', 'The Valley of Fear', this.getDateMinusMinutes(120)),
+        new MailData(4, 'Mrs. Hudson', 'A Scandal in Bohemia', this.getDateMinusMinutes(4320))
     ];
 
     private getDateMinusMinutes(minutes: number): Date {
--- homework/lesson2-directives/src/app/email-date/email-date.pipe.ts
+++ homework/lesson2-directives/src/app/email-date/email-date.pipe.ts
@@ -4,10 +4,22 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'emailDate'
 })
 export class EmailDatePipe implements PipeTransform {
-
-    transform(value: any, args?: any): any {
-        console.log(value);
-        return value;
+    private diffMinutes(date1: Date, date2: Date): number {
+        return Math.abs(Math.round(((date1.getTime() - date2.getTime()) / 1000) / 60));
     }
 
+    transform(value: any, args?: any): any {
+        const dm = this.diffMinutes(value, new Date());
+
+        if (dm < 1.0) { return 'Now'; }
+        if (dm > 1.0 && dm < 60) { return `${dm} minute(s) ago`; }
+        if (dm > 60 && dm < (60 * 24)) {
+            const hours = Math.round(dm / 60);
+            return `${hours} hour(s) ago`;
+        }
+        if (dm > (60 * 24)) {
+            const days = Math.round(dm / (60 * 24));
+            return `${days} day(s) ago`;
+        }
+    }
 }
